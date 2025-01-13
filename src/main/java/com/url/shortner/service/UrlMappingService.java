ec7d5c0c0f373a8d7bcc1ee333cf1daa0cc28183
Tribhuvan nath <tribhuvannath4567@gmail.com>
2025-01-13 13:39:42 +0530

    Completed backend

--- src/main/java/com/url/shortner/service/UrlMappingService.java
+++ src/main/java/com/url/shortner/service/UrlMappingService.java
@@ -86,4 +86,19 @@ public class UrlMappingService {
         return clickEvents.stream()
                 .collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate(), Collectors.counting()));
     }
+
+    public URLMapping getOriginalUrl(String shortUrl) {
+        URLMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        if (urlMapping != null) {
+            urlMapping.setClickCount(urlMapping.getClickCount() + 1);
+            urlMappingRepository.save(urlMapping);
+
+            //Record click Event
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+        return urlMapping;
+    }
 }
