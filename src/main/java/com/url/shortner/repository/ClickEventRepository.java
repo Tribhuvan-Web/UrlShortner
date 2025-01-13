4926c033d02ab277b76d461ebe489166bb22e948
Tribhuvan nath <tribhuvannath4567@gmail.com>
2025-01-13 11:28:40 +0530

    Added count service

---
+++ src/main/java/com/url/shortner/repository/ClickEventRepository.java
@@ -0,0 +1,16 @@
+package com.url.shortner.repository;
+
+import com.url.shortner.models.ClickEvent;
+import com.url.shortner.models.URLMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface ClickEventRepository extends JpaRepository<ClickEvent, Long> {
+    List<ClickEvent> findByUrlMappingAndClickDateBetween(URLMapping urlMapping, LocalDateTime startDate, LocalDateTime endDate);
+    List<ClickEvent> findByUrlMappingInAndClickDateBetween(List<URLMapping> url, LocalDateTime startDate, LocalDateTime endDate);
+
+}
